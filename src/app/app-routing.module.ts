1cebdfac32087f5469e3805ec2d27f4dcc80ec58
kcyap <takeshisean@gmail.com>
2020-05-07 23:36:23 +0800

    updated page with navbar and main content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainContentComponent } from './layout/main-content/main-content.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainContentComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainContentComponent } from './layout/main-content/main-content.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
